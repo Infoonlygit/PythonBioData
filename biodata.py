bc9f187ec27e7dc583d48f49829426920f406af7
Kevin Pandu Samudera (KPS) <162404229+Infoonlygit@users.noreply.github.com>
2024-03-08 00:27:52 +0700

    Update biodata.py

--- biodata.py
+++ biodata.py
@@ -1,11 +1,11 @@
 # biodata.py
 
 # Mendefinisikan variabel-variabel biodata
-- nama = "Kevin Pandu Samudera"
-- umur = "14 tahun"
-- alamat = "Batam, Riau"
-- pekerjaan = "Web Dev"
-- email = "infoonly44@gmail.com"
+nama = "Kevin Pandu Samudera"
+umur = "14 tahun"
+alamat = "Batam, Riau"
+pekerjaan = "Web Dev"
+email = "infoonly44@gmail.com"
 
 # Menampilkan informasi biodata
 print("Nama:", nama)

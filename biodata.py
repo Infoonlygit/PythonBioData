4f9e9b903301451b04a5c466525596cdfcf15ab9
Kevin Pandu Samudera (KPS) <162404229+Infoonlygit@users.noreply.github.com>
2024-03-07 23:48:05 +0700

    Update and rename biomyvinn.github.io to biodata.py

---
+++ biodata.py
@@ -0,0 +1,13 @@
+# biodata.py
+
+# Mendefinisikan variabel-variabel biodata
+nama = "Kevin Pandu Samudera"
+umur = "14 tahun"
+alamat = "Batam, Riau"
+pekerjaan = "Web Dev"
+
+# Menampilkan informasi biodata
+print("Nama:", nama)
+print("Umur:", umur, "tahun")
+print("Alamat:", alamat)
+print("Pekerjaan:", pekerjaan)
